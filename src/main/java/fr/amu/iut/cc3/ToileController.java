f00bc075da8e9460fa95bc7d4d7f762effc54cc1
sandevistan <robin.lerouge@etu.univ-amu.fr>
2023-06-02 15:39:18 +0200

    erreure de saisie

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -13,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
@@ -48,8 +49,10 @@ public class ToileController implements Initializable {
     private TextField compt5= new TextField("");
     @FXML
     private TextField compt6 = new TextField("");
+    @FXML
+    private Label messErr = new Label("");
 
-    private TextField[] ListText = {compt1,compt2};
+    private boolean err = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -68,35 +71,84 @@ public class ToileController implements Initializable {
     @FXML
     public void Trace(){
         //**************************     Compt1     ***********************************
-        double X1 = getXRadarChart(Double.parseDouble(compt1.getText()),1);
-        double Y1 = getYRadarChart(Double.parseDouble(compt1.getText()),1);
-        graphe.getChildren().add(new Circle(X1,Y1,5));
+        if(!compt1.getText().equals("")) {
+            if (Double.parseDouble(compt1.getText()) < 0 || Double.parseDouble(compt1.getText()) > 20){
+                err = true;
+            }
+            else {
+                double X1 = getXRadarChart(Double.parseDouble(compt1.getText()), 1);
+                double Y1 = getYRadarChart(Double.parseDouble(compt1.getText()), 1);
+                graphe.getChildren().add(new Circle(X1, Y1, 5));
+            }
+        }
 
         //**************************     Compt2     ***********************************
-        double X2 = getXRadarChart(Double.parseDouble(compt2.getText()),2);
-        double Y2 = getYRadarChart(Double.parseDouble(compt2.getText()),2);
-        graphe.getChildren().add(new Circle(X2,Y2,5));
 
+        if(!compt2.getText().equals("")) {
+            if (Double.parseDouble(compt2.getText()) < 0 && Double.parseDouble(compt2.getText()) > 20){
+                err = true;
+            }
+            else {
+                double X2 = getXRadarChart(Double.parseDouble(compt2.getText()), 2);
+                double Y2 = getYRadarChart(Double.parseDouble(compt2.getText()), 2);
+                graphe.getChildren().add(new Circle(X2, Y2, 5));
+            }
+        }
         //**************************     Compt3     ***********************************
-        double X3 = getXRadarChart(Double.parseDouble(compt3.getText()),3);
-        double Y3 = getYRadarChart(Double.parseDouble(compt3.getText()),3);
-        graphe.getChildren().add(new Circle(X3,Y3,5));
+        if(!compt3.getText().equals("")) {
+            if (Double.parseDouble(compt3.getText()) < 0 || Double.parseDouble(compt3.getText()) > 20){
+                err = true;
+            }
+            else {
+                double X3 = getXRadarChart(Double.parseDouble(compt3.getText()), 3);
+                double Y3 = getYRadarChart(Double.parseDouble(compt3.getText()), 3);
+                graphe.getChildren().add(new Circle(X3, Y3, 5));
+            }
+        }
 
         //**************************     Compt4     ***********************************
-        double X4 = getXRadarChart(Double.parseDouble(compt4.getText()),4);
-        double Y4 = getYRadarChart(Double.parseDouble(compt4.getText()),4);
-        graphe.getChildren().add(new Circle(X4,Y4,5));
-
+        if(!compt4.getText().equals("")) {
+            if (Double.parseDouble(compt4.getText()) < 0 || Double.parseDouble(compt4.getText()) > 20){
+                err = true;
+            }
+            else {
+                double X4 = getXRadarChart(Double.parseDouble(compt4.getText()), 4);
+                double Y4 = getYRadarChart(Double.parseDouble(compt4.getText()), 4);
+                graphe.getChildren().add(new Circle(X4, Y4, 5));
+            }
+        }
         //**************************     Compt5     ***********************************
-        double X5 = getXRadarChart(Double.parseDouble(compt5.getText()),5);
-        double Y5 = getYRadarChart(Double.parseDouble(compt5.getText()),5);
-        graphe.getChildren().add(new Circle(X5,Y5,5));
+        if(!compt5.getText().equals("")) {
+            if (Double.parseDouble(compt5.getText()) < 0 || Double.parseDouble(compt5.getText()) > 20){
+                err = true;
+            }
+            else {
+                double X5 = getXRadarChart(Double.parseDouble(compt5.getText()), 5);
+                double Y5 = getYRadarChart(Double.parseDouble(compt5.getText()), 5);
+                graphe.getChildren().add(new Circle(X5, Y5, 5));
+            }
+        }
 
         //**************************     Compt6     ***********************************
-        double X6 = getXRadarChart(Double.parseDouble(compt6.getText()),6);
-        double Y6 = getYRadarChart(Double.parseDouble(compt6.getText()),6);
-        graphe.getChildren().add(new Circle(X6,Y6,5));
+        if(!compt6.getText().equals("")) {
+            if (Double.parseDouble(compt6.getText()) < 0 || Double.parseDouble(compt6.getText()) > 20){
+                err = true;
+            }
+            else {
+                double X6 = getXRadarChart(Double.parseDouble(compt6.getText()), 6);
+                double Y6 = getYRadarChart(Double.parseDouble(compt6.getText()), 6);
+                graphe.getChildren().add(new Circle(X6, Y6, 5));
+            }
+        }
 
+        //****************************     Err     *************************************
+        if(err == true){
+            err = false;
+            messErr.setText("Erreur de saisie : \n Les valeurs doivent etre entre 0 et 20");
+        }
+        else{
+            messErr.setText("");
+        }
 
     }
     @FXML
