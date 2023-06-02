461733101f6daa443a02d468cd9ed4e87ae61d7b
sandevistan <robin.lerouge@etu.univ-amu.fr>
2023-06-02 15:09:10 +0200

    tracer des points

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -17,7 +18,9 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -32,6 +35,8 @@ public class ToileController implements Initializable {
     private static int noteMaximale = 20;
 
     @FXML
+    private Pane graphe = new Pane();
+    @FXML
     private TextField compt1= new TextField("");
     @FXML
     private TextField compt2= new TextField("");
@@ -44,6 +49,7 @@ public class ToileController implements Initializable {
     @FXML
     private TextField compt6 = new TextField("");
 
+    private TextField[] ListText = {compt1,compt2};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -61,11 +67,40 @@ public class ToileController implements Initializable {
 
     @FXML
     public void Trace(){
-        
+        //**************************     Compt1     ***********************************
+        double X1 = getXRadarChart(Double.parseDouble(compt1.getText()),1);
+        double Y1 = getYRadarChart(Double.parseDouble(compt1.getText()),1);
+        graphe.getChildren().add(new Circle(X1,Y1,5));
+
+        //**************************     Compt2     ***********************************
+        double X2 = getXRadarChart(Double.parseDouble(compt2.getText()),2);
+        double Y2 = getYRadarChart(Double.parseDouble(compt2.getText()),2);
+        graphe.getChildren().add(new Circle(X2,Y2,5));
+
+        //**************************     Compt3     ***********************************
+        double X3 = getXRadarChart(Double.parseDouble(compt3.getText()),3);
+        double Y3 = getYRadarChart(Double.parseDouble(compt3.getText()),3);
+        graphe.getChildren().add(new Circle(X3,Y3,5));
+
+        //**************************     Compt4     ***********************************
+        double X4 = getXRadarChart(Double.parseDouble(compt4.getText()),4);
+        double Y4 = getYRadarChart(Double.parseDouble(compt4.getText()),4);
+        graphe.getChildren().add(new Circle(X4,Y4,5));
+
+        //**************************     Compt5     ***********************************
+        double X5 = getXRadarChart(Double.parseDouble(compt5.getText()),5);
+        double Y5 = getYRadarChart(Double.parseDouble(compt5.getText()),5);
+        graphe.getChildren().add(new Circle(X5,Y5,5));
+
+        //**************************     Compt6     ***********************************
+        double X6 = getXRadarChart(Double.parseDouble(compt6.getText()),6);
+        double Y6 = getYRadarChart(Double.parseDouble(compt6.getText()),6);
+        graphe.getChildren().add(new Circle(X6,Y6,5));
+
+
     }
     @FXML
     public void Vide(){
-
     }
 
 
